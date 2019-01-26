6ee5a7a9861e1f7dcbb0b41a4955b76e77e8bdeb
Mario <user.name>
2019-01-26 14:59:14 +0100

    update list

--- update.sh
+++ update.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
+
+# if you wanna use it the same way as I do:
+# 1. change the directory that pushd uses
+# 2. change the username
+# 3. add to PATH, or just do an alias to this script.
+# 4. use git config --global credential.helper cache
+
 pushd /mnt/d/Documents/Projects/awesome-stars/
 echo Regenerating README.md...
 python3 -m starred.starred --username dizaztor --sort > ./README.md
@@ -8,3 +15,4 @@ git commit -m "update list"
 git push origin master
 echo Done.
 popd
+
