64ab07b2beb0b6918681035c575ec747ece37893
Mario <user.name>
2019-01-26 14:54:05 +0100

    update list

--- update.sh
+++ update.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
-python3 -m starred.starred --username dizaztor --sort > README.md
+pushd /mnt/d/Documents/Projects/awesome-stars/
+echo Regenerating README.md...
+python3 -m starred.starred --username dizaztor --sort > ./README.md
+echo Updating the repo...
 git add -A
 git commit -m "update list"
 git push origin master
+echo Done.
+popd
